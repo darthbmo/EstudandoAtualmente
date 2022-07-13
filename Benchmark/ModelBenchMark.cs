3d75226ef6f721adc3a049904b85aea5d1b46096
darthbmo <connectionreverse@gmail.com>
2022-07-13 09:26:32 -0300

    Testes na lib Benchmark.

---
+++ Benchmark/ModelBenchMark.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace EstudosGerais.Benchmark
+{
+    public class ModelBenchMark
+    {
+        [Benchmark]
+        public int QuantasVezes()
+        {
+            var total = 0;
+
+            for (var i = 0; i < 100; i++)
+            {
+                total += i;
+            }
+
+            return total;
+        }
+    }
+}
